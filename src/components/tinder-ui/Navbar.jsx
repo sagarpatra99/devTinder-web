b0cc3dac087e380cb60dd91ac4f3d3e962a9d911
sagarpatra99 <sagarpatra641@gmail.com>
2025-07-12 14:56:54 +0530

    first commit

---
+++ src/components/tinder-ui/Navbar.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../../../public/assets/logo.png";
+import profile from "../../../public/assets/profileImg.png";
+
+export default function Navbar() {
+  return (
+    <div className="bg-gray-800 text-white py-4 px-6 flex items-center justify-between">
+      <Link href={"/"}>
+        <div className="flex items-center text-xl">
+          <Image src={logo} width={60} alt="logo" />
+          <span className="font-extrabold text-[#C7DD25]">&nbsp;DEV</span><span className="text-[#00ACE8]">Tinder</span>
+        </div>
+      </Link>
+      <div>
+        <Image src={profile} width={70} alt="Picture of the author" />
+      </div>
+    </div>
+  );
+}
